a73d3a2acc1a6f1e3694bb0cf832f03de5e26c54
KavithaSubra <96657989+KavithaSubra@users.noreply.github.com>
2021-12-26 09:54:37 +0530

    Change in Print statement

--- Factorial.java
+++ Factorial.java
@@ -2,10 +2,10 @@ package week1.day1;
 
 public class Factorial {
 public static void main(String[] args) {
-	int fact=1;
-	for(int i=1; i<=5;i++)
+	int fact=1,n=5;
+	for(int i=1; i<=n;i++)
 		fact=fact*i;
-	System.out.println(fact);
+	System.out.println("Factorial of " + n + " is " + fact);
 	 
 }
 }
